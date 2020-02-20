5a87b129e867bb22266d506b3676ceae689c3de5
Mana <mana_ohm@hotmail.com>
2020-02-20 15:41:57 -0500

    fix bug in sorting book score

--- qualification/greedy_by_book_per_day.py
+++ qualification/greedy_by_book_per_day.py
@@ -1,18 +1,14 @@
 from util import Answer
 
 
-def cal(books, libs):
+def cal(books, libs, n_day):
     def sort_by_signup_day(lib):
         return -lib.book_per_day, lib.signup_day
 
-    sort_keys = [
-        sort_by_signup_day,
-    ]
+    sorted_libs = sorted(libs, key=sort_by_signup_day)
     answers = []
-    for key in sort_keys:
-        sorted_libs = sorted(libs, key=key)
-        for lib in sorted_libs:
-            ans = Answer(lib.id, sorted(lib.books, key=lambda book: book.score))
-            answers.append(ans)
+    for lib in sorted_libs:
+        ans = Answer(lib, sorted(lib.books, key=lambda book: -book.score))
+        answers.append(ans)
 
     return answers
--- qualification/greedy_by_sum_book_score.py
+++ qualification/greedy_by_sum_book_score.py
@@ -1,14 +1,14 @@
 from util import Answer
 
 
-def cal(books, libs):
+def cal(books, libs, n_day):
     def sort_by_signup_day(lib):
         return sum(book.score for book in lib.books), -len(lib.books), -lib.book_per_day, lib.signup_day
 
     answers = []
     sorted_libs = sorted(libs, key=sort_by_signup_day)
     for lib in sorted_libs:
-        ans = Answer(lib.id, sorted(lib.books, key=lambda book: book.score))
+        ans = Answer(lib.id, sorted(lib.books, key=lambda book: -book.score))
         answers.append(ans)
 
     return answers
